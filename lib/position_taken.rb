2700b75c942fdbc9b5d8a6daf8f673ab19e79963
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-03 18:12:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == ' ' || board[index] == '' || board[index] == nil
-    false
+    return false
   end
   
   if board[index] == 'X' || board[index] == 'O'
-    true
+    return true
   end
 end
